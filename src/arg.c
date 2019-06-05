6bd30317e687d2586f53689a268723417e054135
Tony Arick <maa146@Maa146-linux.HPC.MsState.Edu>
2019-06-05 08:28:10 -0500

    Convert arg parsing to klib

--- src/arg.c
+++ src/arg.c
@@ -4,7 +4,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "ketopt.h"
+
 const char* const program_version = "quack 2.0";
+
+const char* const help_message = ""
+  "Usage: quack [OPTION...]\n"
+  "quack -- A FASTQ quality assessment tool\n\n"
+  "  -1, --forward file.1.fq.gz      Forward strand\n"
+  "  -2, --reverse file.2.fq.gz      Reverse strand\n"
+  "  -a, --adapters adapters.fa.gz   (Optional) Adapters file\n"
+  "  -n, --name NAME                 (Optional) Display in output\n"
+  "  -u, --unpaired unpaired.fq.gz   Data (only use with -u)\n"
+  "  -?, --help                      Give this help list\n"
+  "      --usage                     (use alone)\n"
+  "  -V, --version                   Print program version (use alone)\n"
+  "Report bugs to <thrash@igbb.msstate.edu>.\n";
+
+static ko_longopt_t longopts[] = {
+    { "forward",  ko_required_argument, '1' },
+    { "reverse",  ko_required_argument, '2' },
+    { "unpaired", ko_required_argument, 'u' },
+    { "adapters", ko_required_argument, 'a' },
+
+    { "name",     ko_required_argument, 'n' },
+
+    { "help",    ko_no_argument, 'h' },
+    { "usage",   ko_no_argument, 'h' },
+    { "version", ko_no_argument, 'V' },
+
+    { NULL, 0, 0 }
+  };
 
 struct arguments parse_options(int argc, char **argv) {
   struct arguments arguments = {
@@ -15,66 +45,31 @@ struct arguments parse_options(int argc, char **argv) {
                                 .adapters = NULL
   };
 
-    if (argc== 1 || argc == 2)  {
-        if (argc == 1 || (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "--usage") == 0 || strcmp(argv[1], "-?") == 0)) {
-            printf("Usage: quack [OPTION...]\n"
-            "quack -- A FASTQ quality assessment tool\n\n"
-            "  -1, --forward file.1.fq.gz      Forward strand\n"
-            "  -2, --reverse file.2.fq.gz      Reverse strand\n"
-            "  -a, --adapters adapters.fa.gz   (Optional) Adapters file\n"
-            "  -n, --name NAME                 (Optional) Display in output\n"
-            "  -u, --unpaired unpaired.fq.gz   Data (only use with -u)\n"
-            "  -?, --help                      Give this help list\n"
-            "      --usage                     (use alone)\n"
-            "  -V, --version                   Print program version (use alone)\n"
-            "Report bugs to <thrash@igbb.msstate.edu>.\n");
-        }
 
-        if (argc == 2 && ((strcmp(argv[1], "-V") == 0 || strcmp(argv[1], "--version") == 0))) {
-            printf("%s\n", program_version);
-            exit (0);
-        }
-    }
+  ketopt_t opt = KETOPT_INIT;
 
-    if(argc>2 && argc % 2 != 0)
-    {
-        int counter=1;
-        while (counter<argc) {
-            if (strcmp(argv[counter], "--forward") == 0 || strcmp(argv[counter], "-1") == 0) {
-                arguments.forward = argv[counter+1];
-            }
+  int i, c;
+  while ((c = ketopt(&opt, argc, argv, 1, "1:2:a:u:n:?V", longopts)) >= 0) {
+    switch(c){
+    case '1': arguments.forward  = opt.arg; break;
+    case '2': arguments.reverse  = opt.arg; break;
+    case 'u': arguments.unpaired = opt.arg; break;
+    case 'a': arguments.adapters = opt.arg; break;
 
-            else if (strcmp(argv[counter], "--reverse") == 0 || strcmp(argv[counter], "-2") == 0) {
-                arguments.reverse = argv[counter+1];
-            }
+    case 'n': arguments.name = opt.arg;     break;
 
-            else if (strcmp(argv[counter], "--adapters") == 0 || strcmp(argv[counter], "-a") == 0) {
-                arguments.adapters = argv[counter+1];
-            }
+    case 'V':
+      printf("%s\n", program_version);
+      exit(EXIT_SUCCESS);
 
-            else if (strcmp(argv[counter], "--unpaired") == 0 || strcmp(argv[counter], "-u") == 0) {
-                arguments.unpaired = argv[counter+1];
-            }
+    case 'h':
+    case '?':
+    default:
+      printf("%s\n", help_message);
+      exit(EXIT_SUCCESS);
 
-            else if (strcmp(argv[counter], "--name") == 0 || strcmp(argv[counter], "-n") == 0) {
-                arguments.name = argv[counter+1];
-            }
-            else {
-                printf("Usage: quack [OPTION...]\n"
-                "quack -- A FASTQ quality assessment tool\n\n"
-                "  -1, --forward file.1.fq.gz      Forward strand\n"
-                "  -2, --reverse file.2.fq.gz      Reverse strand\n"
-                "  -a, --adapters adapters.fa.gz    Adapters file\n"
-                "  -n, --name NAME            Display in output\n"
-                "  -u, --unpaired unpaired.fq.gz        Data (only use with -u)\n"
-                "  -?, --help                 Give this help list\n"
-                "      --usage                (use alone)\n"
-                "  -V, --version              Print program version (use alone)\n"
-                "Report bugs to <thrash@igbb.msstate.edu>.\n");
-            }
+    };
+  }
 
-            counter = counter+2;
-        }
-    }
     return arguments;
 }
